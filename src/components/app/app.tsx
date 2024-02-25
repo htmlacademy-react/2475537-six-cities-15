f65a7602b8d64423ac0d1d187db68eeb11f23269
Petr Zaretsky <Petr.Zaretsky@pluritech.com>
2024-02-25 14:50:31 +0300

    - implement routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,52 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import Layout from '../layout/layout';
+import PrivateRoute from '../privateRoute/privateRoute';
 import Main from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import NotFound from '../../pages/notFound/notFound';
+import Offer from '../../pages/offer/offer';
 
 type AppProps = {
   cardsCount: number;
 };
 
 function App({ cardsCount }: AppProps) {
-  return <Main cardsCount={cardsCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Layout />}
+        >
+          <Route
+            index
+            element={<Main cardsCount={cardsCount} />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<Offer />}
+          />
+        </Route>
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
