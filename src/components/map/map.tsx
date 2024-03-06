6ec8a3ae0dbd42099b2aecf91645302a5bc4b48e
Petr Zaretsky <Petr.Zaretsky@pluritech.com>
2024-03-06 18:28:51 +0300

    try pass build

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -13,7 +13,7 @@ type MapProps = {
 function Map({ offers, activeOffer, height }: MapProps) {
   const [firstOffer] = offers;
 
-  const mapRef = useRef<MutableRefObject<string | HTMLElement>>(null);
+  const mapRef = useRef<MutableRefObject<string | HTMLElement> | null>(null);
   const map = useMap(mapRef, {
     lat: firstOffer.coords.lat,
     lng: firstOffer.coords.lng,
