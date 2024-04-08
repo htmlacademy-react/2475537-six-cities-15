2861dfcc385124a1c3d4b31a4efe6e667314419d
Petr Zaretsky <Petr.Zaretsky@pluritech.com>
2024-04-08 07:33:02 +0300

    fix

--- src/components/editabeRating/editableRating.test.tsx
+++ src/components/editabeRating/editableRating.test.tsx
@@ -2,8 +2,10 @@ import { render, screen } from '@testing-library/react';
 import EditableRating from './editableRating';
 
 describe('EditableRating', () => {
+  const onRatingChanged = () => { };
+
   it('should render EditableRating component', () => {
-    render(<EditableRating onRatingChanged={() => void} value={2}/>);
+    render(<EditableRating onRatingChanged={onRatingChanged} value={2}/>);
 
     expect(screen.getByTestId('rating-wrapper')).toBeInTheDocument();
   });
--- src/components/editabeRating/star.test.tsx
+++ src/components/editabeRating/star.test.tsx
@@ -2,14 +2,16 @@ import { render, screen } from '@testing-library/react';
 import Star from './star';
 
 describe('Star', () => {
+  const onRatingChanged = () => { };
+
   it('should render Star component', () => {
-    render(<Star rating={1} value={2} onRatingChanged={() => void} />);
+    render(<Star rating={1} value={2} onRatingChanged={onRatingChanged} />);
 
     expect(screen.getByTestId('rating-star')).toBeInTheDocument();
   });
 
   it('should render Star checked component', () => {
-    render(<Star rating={2} value={2} onRatingChanged={() => void} />);
+    render(<Star rating={2} value={2} onRatingChanged={onRatingChanged} />);
 
     expect(screen.getByTestId('rating-star')).toBeChecked();
   });
