20e93f5c96de35faa06a60c90d86e231707d7418
Petr Zaretsky <Petr.Zaretsky@pluritech.com>
2024-04-09 07:47:26 +0300

    fix eslint

--- src/components/reviewList/reviewList.test.tsx
+++ src/components/reviewList/reviewList.test.tsx
@@ -19,7 +19,7 @@ describe('ReviewList', () => {
   const offerId = 'id1';
   const requestUrl = APIRoutes.Comments.replace('{offerId}', offerId);
 
-  it('should fetch reviews on ReviewList component', async () => {
+  it('should fetch reviews on ReviewList component', () => {
     const { withStoreComponent, mockStore, mockAxiosAdapter } = withStore(<ReviewList offerId={offerId} />, makeFakeStore());
     mockAxiosAdapter.onGet(requestUrl).reply(200);
 
@@ -31,20 +31,20 @@ describe('ReviewList', () => {
     ]);
   });
 
-  it('should render ReviewList component with reviews', async () => {
+  it('should render ReviewList component with reviews', () => {
     const { withStoreComponent } = withStore(<ReviewList offerId={offerId} />, makeFakeStore({
       [Namespace.Data]: {
         offerReviews: [makeFakeReview(), makeFakeReview()],
         isOfferReviewsLoading: false,
       }
     }));
-    
+
     render(withStoreComponent);
 
     expect(screen.getAllByText('ReviewCard').length).toBe(2);
   });
 
-  it('should render ReviewList component with reviewForm', async () => {
+  it('should render ReviewList component with reviewForm', () => {
     const { withStoreComponent } = withStore(<ReviewList offerId={offerId} />, makeFakeStore({
       [Namespace.User]: {
         authorizationStatus: AuthorizationStatus.Auth,
